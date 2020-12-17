7eaa146ee6ef136703e92907a59e362fbc4a0373
Marco Aurelio <marcocpdti@gmail.com>
2020-12-17 00:36:42 -0300

    Social media links set

--- src/components/socials/index.js
+++ src/components/socials/index.js
@@ -6,16 +6,16 @@ import { Container } from './styles';
 function socials() {
     return (
         <Container>
-            <a href="#" >
+            <a href="https://www.facebook.com/LADO-HOSTIL-Tributo-ORappa-293628441150691" >
                 <Image src="/images/icons/facebook.png" width={40} height={40} />
             </a>
-            <a href="#" >
+            <a href="https://www.instagram.com/ladohostil/" >
                 <Image src="/images/icons/instagram.png" width={40} height={40} />
             </a>
             <a href="#" >
                 <Image src="/images/icons/whatsapp.png" width={40} height={40} />
             </a>
-            <a href="#" >
+            <a href="https://www.youtube.com/channel/UCJbYNv36IXpK-SG2kDGtVlQ" >
                 <Image src="/images/icons/youtube.png" width={40} height={40} />
             </a>
         </Container>
