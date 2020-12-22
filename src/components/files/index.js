591cd1e2207966ff403f41977c4d7e493b9eae39
Marco Aurelio <marcocpdti@gmail.com>
2020-12-21 21:54:08 -0300

    Replaced files component css and arrow function

--- src/components/files/index.js
+++ src/components/files/index.js
@@ -1,19 +1,19 @@
+import { faCalendarTimes } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
+import styles from './styles.module.css';
 
-import { Container, Files } from './styles';
 
-function files() {
+const files = ()=> {
     return (
-        <Container>
+        <div className={styles.container}>
             <span id="files">Baixe os arquivos da banda Lado Hostil</span>
-            <Files>
+            <div className={styles.containerFiles}>
                 <div><a src="#">Release</a></div>
                 <div><a src="#">Mapa de palco</a></div>
                 <div><a src="#">Rider Técnico</a></div>
                 <div><a src="#">Release</a></div>
-            </Files>
-        </Container>
-    )
-}
+            </div>
+        </div>
+)} 
 
 export default files;
