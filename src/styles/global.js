b348b24baae5adcb904d4f546d6920f26f2f634e
Marco Aurelio <marcocpdti@gmail.com>
2020-12-21 22:59:27 -0300

    Fixing general errors

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,7 +1,9 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-    /* @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap'); */
+    /* @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap'); */ 
+    /* substituir import das fontes do google */
+    
     * {
         margin: 0;
         padding: 0;
