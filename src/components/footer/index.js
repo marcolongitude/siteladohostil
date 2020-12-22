ff2afa273bfca3af213fb4a894e98762316a5234
Marco Aurelio <marcocpdti@gmail.com>
2020-12-21 23:45:41 -0300

    Footer component initial development

---
+++ src/components/footer/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Image from 'next/image';
+import styles from './styles.module.css';
+
+const footer = () => {
+    return  (
+        <div className={styles.container}>
+
+            <div className={styles.containerLogo}>
+                <Image src="/images/logos/ladohostilpretobranco.png" width={130} height={130} />
+            </div>
+
+            <div className={styles.containerSocials}>
+                <a href="https://www.facebook.com/LADO-HOSTIL-Tributo-ORappa-293628441150691" >
+                    <Image src="/images/icons/facebook.png" width={40} height={40} />
+                </a>
+                <a href="https://www.instagram.com/ladohostil/" >
+                    <Image src="/images/icons/instagram.png" width={40} height={40} />
+                </a>
+                <a href="#" >
+                    <Image src="/images/icons/whatsapp.png" width={40} height={40} />
+                </a>
+                <a href="https://www.youtube.com/channel/UCJbYNv36IXpK-SG2kDGtVlQ" >
+                    <Image src="/images/icons/youtube.png" width={40} height={40} />
+                </a>
+            </div>
+
+            <div className={styles.informationsFooter}>
+                <p>Anthonielly ( tony ) - vocal</p>
+                <p>Madson ( madim ) - guitarra </p>
+                <p>Murilo  ( gerera ) - baixo</p>
+                <p>Marco Aurélio ( marco ) - guitarra</p>
+            </div>
+        </div>
+    )
+}
+export default footer;
